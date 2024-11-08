22f67b5fb3f32587f1705315fc80ad7ee5543400
Celina Lind <87724360+celinaLind@users.noreply.github.com>
2024-11-07 19:39:54 -0800

    Create Contains-Duplicate.py

---
+++ Review/Contains-Duplicate.py
@@ -0,0 +1,41 @@
+class FirstWrongSolution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+(*         Count instances of a value w/in array *)
+(*         # if found two or more instances return true *)
+(*         # else return false *)
+
+(*     # time limit  *)
+        for number in nums:
+            count = nums.count(number)
+            if count >= 2:
+                return True
+        
+        return False
+
+(*   Received an exceeded time limit error.
+  During interview ask:
+  - is there a limit to the amount of values in the array being tested
+
+NOTES:
+Time complexity is O(n^2)
+This is b/c count() fcn iterates through the array again to find the number of counts of a value [count() has a time complexity of O(n)
+
+It is inefficient to use a fcn with time complexity of O(n^2) if the input list nums has a large size (e.g., n = 10^5 or more)
+
+*)
+
+class CorrectSolution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+(*         # Solution 1: sort and compare *)
+        nums.sort() # this sorts the list into asc. order
+
+(*         # go through list and compare number to previous number
+        # if they are equal return true if not continue *)
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        
+        return False
+
+(*         complexity of O(n) *)
